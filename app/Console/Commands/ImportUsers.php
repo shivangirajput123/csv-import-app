53f2e02760536cfb3b3d43fa46fe7cc2eaa8586a
Bhargav naidu <bhargavmacbookpro@Bhargavs-MacBook-Pro.local>
2026-04-09 19:27:40 +0530

    project added

---
+++ app/Console/Commands/ImportUsers.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('app:import-users')]
+#[Description('Command description')]
+class ImportUsers extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    protected $signature = 'app:import-users';
+    protected $description = 'Import users from a CSV file';
+
+    public function handle()
+    {
+        try{
+            $file = fopen(storage -> path ('app/public/users.csv'), 'r');
+            $headers = fgetcsv($file);
+            while(($row = fgetcsv($file)) !== false){
+                $username = $row[0];
+                $email = $row[1];
+                $password = $row[2];
+                
+                if(!filter_var ($email, FILTER_VALIDATE_EMAIL)){
+                    $this->error("Invalid email formar for user: $username");
+                    continue;
+                }
+
+                if(!preg_match('/^(a-zA-Z0-9) {3,20} $', $username)){
+                    $this->error("Invalid username format for user: $username");
+                }
+            }
+
+             \App\Models\User::create([
+            'name' => $username,
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        fclose($file);
+        $this->info('User import completed successfully.');
+        } catch (\Exception $e){
+            $this->error('An error occurred: ' . $e->getMessage());
+        }
+    
+    }
+
+}
